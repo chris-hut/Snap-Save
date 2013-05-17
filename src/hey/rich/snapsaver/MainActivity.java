6d5604267ccca3e0ecac5301973e1a3e00c729b8
Chris <tophernuts@gmail.com>
2013-05-17 00:09:42 -0700

    Alpha release. .java and Manifest files. App works fine on my test phone: Can copy and rename snap chat pictures.

--- src/hey/rich/snapsaver/MainActivity.java
+++ src/hey/rich/snapsaver/MainActivity.java
@@ -1,6 +1,8 @@
 package hey.rich.snapsaver;
 
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 
 import android.app.Activity;
@@ -23,27 +25,27 @@ public class MainActivity extends Activity {
 	private Button buttonPicture;
 	private Button buttonVideo;
 	private Button buttonBoth;
-	
-	// Strings 
+
+	// Strings
 	private String storageLocation;
 	private static final String STRING_PREFERENCES = "stringPrefs";
-	
+
 	// Shared Preferences
 	private SharedPreferences prefs;
 	private SharedPreferences.Editor prefEditor;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		prefs = getSharedPreferences(STRING_PREFERENCES, MODE_PRIVATE);
 		prefEditor = prefs.edit();
 
 		buttonPicture = (Button) findViewById(R.id.button_copy_picture);
 		buttonVideo = (Button) findViewById(R.id.button_copy_video);
 		buttonBoth = (Button) findViewById(R.id.button_both);
-		
+
 		this.buttonOnClick();
 	}
 
@@ -54,7 +56,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// Copy Pictures
-				copyDirectory("picture");
+				copyDirectory("/pictures/");
 				// Rename Pictures
 				renameAllFiles("/pictures/");
 			}
@@ -65,7 +67,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// Copy videos
-				copyDirectory("video");
+				copyDirectory("/videos/");
 				// Rename videos
 				renameAllFiles("/videos/");
 			}
@@ -80,21 +82,24 @@ public class MainActivity extends Activity {
 		});
 	}
 
-	/** Copy files to new directory 
+	/**
+	 * Copy files to new directory
 	 * 
 	 * Code modifed from:
-	 * http://stackoverflow.com/questions/10735273/copy-folders-in-data-data-to-sdcard-viceversa
+	 * http://stackoverflow.com/questions/10735273/copy-folders
+	 * -in-data-data-to-sdcard-viceversa
 	 */
 	private void copyDirectory(String type) {
 		String toastText = "";
 		String copyString = "cp ";
 
-		if (type.equals("picture")) {
+		if (type.equals("/pictures/")) {
 			toastText = getString(R.string.progress_1_copy_picture)
 					+ getString(R.string.location_pictures);
 			// TODO: Don't make this a hardcoded string huh
-			copyString = copyString + "/data/data/com.snapchat.android/cache/received_image_snaps/* ";
-		} else if (type.equals("video")) {
+			copyString = copyString
+					+ "/data/data/com.snapchat.android/cache/received_image_snaps/* ";
+		} else if (type.equals("/videos/")) {
 			toastText = getString(R.string.progress_1_copy_video)
 					+ getString(R.string.location_videos);
 		}
@@ -102,58 +107,124 @@ public class MainActivity extends Activity {
 		// Toast the copy
 		Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT)
 				.show();
-		
+
 		// Add their storage location
-		copyString = copyString + storageLocation + getString(R.string.location_pictures);
+		copyString = copyString + storageLocation + type;
+
+		Log.d("Snaps_Copy", "CopyString: " + copyString);
+
 		// Do copy
-		// TODO: Need to somehow figure out how many files were copied, or if no files were copied
-		try{
+		// TODO: Need to somehow figure out how many files were copied, or if no
+		// files were copied
+		try {
 			Process suProcess = Runtime.getRuntime().exec("su");
-			DataOutputStream os = new DataOutputStream(suProcess.getOutputStream());
-			
+			DataOutputStream os = new DataOutputStream(
+					suProcess.getOutputStream());
+
 			os.writeBytes(copyString + "\n");
 			os.flush();
 			os.writeBytes("exit\n");
 			os.flush();
 			int suProcessRetval = suProcess.waitFor();
-			
-			if (suProcessRetval != 255)
-			{
+
+			if (suProcessRetval != 255) {
 				// We were given root
-			}else
-			{
+			} else {
 				// No root :(
 			}
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
-		}catch(InterruptedException e)
-		{
+		} catch (InterruptedException e) {
 			Log.w("hey.rich.snapsaver", "Error getting root.");
 			e.printStackTrace();
 		}
 	}
 
-	/** Rename all of the files */
+	/**
+	 * Rename all of the files
+	 * 
+	 * Modified from: Modified from:
+	 * http://www.tutorialspoint.com/java/io/file_listfiles_file_filter.htm
+	 */
 	private void renameAllFiles(String directory) {
 		String toastText = "";
 		String renameString ="rn ";
-		
+		String tempRename;
+		File f;
+		File[] files;
+
+		FileFilter filter = new FileFilter() {
+			@Override
+			public boolean accept(File pathName) {
+				// File must exist
+				if (!pathName.isFile()) {
+					Log.d("Snaps_Rename", "File does not exist");
+					Log.d("Snaps_Rename", "File name was: " + pathName.getAbsolutePath());
+					return false;
+				}
+				// File must end in .nomedia
+				if (!pathName.getAbsolutePath().substring(
+						pathName.getAbsolutePath().length() - 8).equals(".nomedia")) {
+					Log.d("Snaps_Rename", "File did not end in .nomedia");
+					Log.d("Snaps_Rename", "File name was: " + pathName.getAbsolutePath());
+					return false;
+				}
+				/*
+				 * File must be like: h1a81hurcs00h1368487198510.jpg.nomedia
+				 */
+				// TODO: Find a way to use directory that is passed into this method
+				if (!pathName.getAbsolutePath().matches(storageLocation + "/pictures/" + "h1a81hurcs00h[0-9]{13,}.jpg.nomedia")) {
+					Log.d("Snaps_Rename", "File wasn't cool looking");
+					Log.d("Snaps_Rename", "File name was: " + pathName.getAbsolutePath());
+					return false;
+				}
+				return true;
+			}
+		};
+
 		if (directory.equals("/pictures/")) {
 			toastText = getString(R.string.progress_2_rename_picture)
-					+ storageLocation
-					+ getString(R.string.location_pictures);
+					+ storageLocation + "/pictures/";
 			// TODO: Don't make this a hardcoded string
 			renameString = renameString + "";
+
 		} else if (directory.equals("/videos/")) {
 			toastText = getString(R.string.progress_2_rename_video)
 					+ storageLocation
-					+ getString(R.string.location_videos);
+					+ "/videos/";
+
+		}
+		try{
+			f = new File(storageLocation + directory);
+
+			files = f.listFiles(filter);
+
+			// Toast the rename
+			Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT)
+			.show();
+			// Do rename
+			for(File path:files)
+			{
+				tempRename = path.getAbsolutePath();
+
+				Log.d("Snaps_Rename", "Old name: " + tempRename);
+
+				tempRename = tempRename.substring(0, tempRename.length() - 8);
+
+				Log.d("Snaps_Rename", "New name: " + tempRename);
+
+				if(!path.renameTo(new File(tempRename)))
+				{
+					Log.d("Snaps_Rename", "File was not renamed correctly.");
+				}
+				
+				Log.d("Snaps_Rename", "Actual new name is: " + path.getAbsolutePath());
+			}
+		} catch(NullPointerException e)
+		{
+			e.printStackTrace();
 		}
 
-		// Toast the copy
-		Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT)
-				.show();
-		// Do copy
 	}
 
 	@Override
@@ -176,68 +247,70 @@ public class MainActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
 	/**
 	 * Creates a dialog to allow the user to change their current directory
 	 * 
 	 * Modified from mkoyong:
 	 * http://www.mkyong.com/android/android-custom-dialog-example/
 	 * */
-	private void changeDirectory(Context context)
-	 {
-		 final Dialog dialog = new Dialog(context);
-		 dialog.setContentView(R.layout.change_directory);
-		 dialog.setTitle(R.string.dialog_new_directory);
-		 
-		 // Set the custom dialog components
-		 final EditText text = (EditText) dialog.findViewById(R.id.dialog_edit_text);
-		 
-		 // Setting text to be what every it is
-		 text.setText(storageLocation);
-		 Button saveButton = (Button) dialog.findViewById(R.id.dialog_positive_button);
-		 Button cancelButton = (Button) dialog.findViewById(R.id.dialog_negative_button);
-		 
-		 saveButton.setOnClickListener(new OnClickListener() {
-			
+	private void changeDirectory(Context context) {
+		final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.change_directory);
+		dialog.setTitle(R.string.dialog_new_directory);
+
+		// Set the custom dialog components
+		final EditText text = (EditText) dialog
+				.findViewById(R.id.dialog_edit_text);
+
+		// Setting text to be what every it is
+		text.setText(storageLocation);
+		Button saveButton = (Button) dialog
+				.findViewById(R.id.dialog_positive_button);
+		Button cancelButton = (Button) dialog
+				.findViewById(R.id.dialog_negative_button);
+
+		saveButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO: Save the text
 				storageLocation = text.getText().toString();
-				
+
 				// Dismiss the dialog
 				dialog.dismiss();
-				
+
 				// Set the EditText to be the storageLocation text
 				text.setText(storageLocation);
 			}
 		});
-		 
-		 cancelButton.setOnClickListener(new OnClickListener() {
-			
+
+		cancelButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// Dismiss the dialog
 				dialog.dismiss();
 			}
 		});
-		 
-		 dialog.show();
-	 }
-	
+
+		dialog.show();
+	}
+
 	@Override
-	protected void onPause()
-	{
+	protected void onPause() {
 		super.onPause();
-		prefs = this.getSharedPreferences("hey.rich.SnapSaver", Context.MODE_PRIVATE);
+		prefs = this.getSharedPreferences("hey.rich.SnapSaver",
+				Context.MODE_PRIVATE);
 		prefEditor.putString("storageLocation", storageLocation);
 		prefEditor.commit();
 	}
-	
+
 	@Override
-	protected void onResume()
-	{
+	protected void onResume() {
 		super.onResume();
-		storageLocation = prefs.getString("storageLocation", getString(R.string.default_location));
-		
+		storageLocation = prefs.getString("storageLocation",
+				getString(R.string.default_location));
+
 	}
 }
